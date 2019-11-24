4be0b1998c1b10ad572aad42cbaad75ca4d7e616
xxristoskk <xristos.kvtsvros@gmail.com>
2019-11-24 18:24:29 -0500

    still fixing setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/echo "\
 [general]\n\
 email = \"xristos.kvtsvros@gmail.com\"\n\
 " > ~/.streamlit/credentials.tomlecho "\
-echo "\
+
 [server]\n\
 headless = true\n\
 port = $PORT\n\
