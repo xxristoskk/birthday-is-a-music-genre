0956dd73bb49e5142cf27f7f6ceab7de41426142
xxristoskk <xristos.kvtsvros@gmail.com>
2019-11-24 17:58:16 -0500

    setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlit/echo "\
+[general]\n\
+email = \"email@website.com\"\n\
+" > ~/.streamlit/credentials.tomlecho "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
